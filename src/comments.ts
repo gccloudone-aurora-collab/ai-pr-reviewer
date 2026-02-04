0edade03ddf0c4057cecdc1c579c34e96afb7d51
John Bain <john.bain@gmail.com>
2026-02-04 14:55:56 -0500

    fix for lost threads

--- src/comments.ts
+++ src/comments.ts
@@ -5,7 +5,7 @@ export type ReviewComment = {
   path: string;
   body: string;
   diff_hunk?: string;
-  line?: number;
+  line?: number | null;
   in_reply_to_id?: number;
   id: number;
   start_line?: number | null;
@@ -65,7 +65,7 @@ export function isThreadRelevant(thread: ReviewCommentThread): boolean {
 
 function generateCommentThreads(reviewComments: ReviewComment[]): ReviewCommentThread[] {
   const topLevelComments = reviewComments.filter(
-    (c) => !c.in_reply_to_id && c.body.length && !!c.line,
+    (c) => !c.in_reply_to_id && c.body.length,
   );
 
   return topLevelComments.map((topLevelComment) => {
